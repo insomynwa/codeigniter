d5b7eca7d8c5fcab99ee3a993fb694c80f10c832
neptunus <pikamons@gmail.com>
2012-12-28 16:21:03 +0700

    3rd commit
    template

--- application/views/pages/template.php
+++ application/views/pages/template.php
@@ -1,9 +1,54 @@
 <html>
 <head>
 	<title><?php echo $judul ?></title>
+	<style type="text/css">
+		body {
+			background: black;
+		}
+		#wrapper {
+			width: 900px;
+			background: pink;
+			margin: 10px auto 0 auto;
+		}
+		#banner {
+			background: blue;
+			height: 200px;
+			margin:0;
+		}
+		#navi {
+			background: green;
+			width: 250px;
+			float: left;
+		}
+			#navi a {
+				color: white;
+				text-decoration: none;
+				font-size: 18pt;
+			}
+		#main {
+			background: #eeeeee;
+			height: 640px;
+			margin: 0 10px 10px 260px;
+		}
+		#footer {
+			background: yellow;
+		}
+	</style>
 </head>
 <body>
-	<p><a href="<?php echo base_url(); ?>">home</a></p>
-	<?php $this->load->view('pages/'.$hal); ?>
+	<div id="wrapper"><!-- wrapper -->
+		<div id="banner"><!-- banner -->
+			header
+		</div>
+		<div id="navi"><!-- navigasi -->
+			<a href="<?php echo base_url(); ?>">home</a>
+		</div>
+		<div id="main"><!-- main -->
+			<?php $this->load->view('pages/'.$hal); ?>
+		</div>
+		<div id="footer"><!-- footer -->
+			footer
+		</div>
+	</div>
 </body>
 </html>
